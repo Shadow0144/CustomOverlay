a941d465110350dd4c981db357d26bd0013b6ad9
Shadow0144 <CorbinCogswell@gmail.com>
2021-02-24 19:34:34 -0600

    Initial commit

---
+++ CustomOverlayMod/Patches.cs
@@ -0,0 +1,20 @@
+﻿using Harmony;
+
+namespace CustomOverlayMod
+{
+    public class Patches
+    {
+        [HarmonyPatch(typeof(Db))]
+        [HarmonyPatch("Initialize")]
+        public class Db_Initialize_Patch
+        {
+            public static void Postfix()
+            {
+                Debug.Log("Adding overlay options...");
+
+
+                Debug.Log("...Overlay options added!");
+            }
+        }
+    }
+}
